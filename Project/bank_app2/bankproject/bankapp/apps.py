c4ee791273bf1cd52851a8368f08e8bdc4f3c946
parthiv-dave <u24ai082@aid.svnit.ac.in>
2025-04-29 20:36:03 +0530

    Delete Project/bank_app2/bankproject directory

--- Project/bank_app2/bankproject/bankapp/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class BankappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bankapp'
-def ready(self):
-    import bankapp.signals
